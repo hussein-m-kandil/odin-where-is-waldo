3c5192447d7a9fa1c8702cd84f9c1541f17dac0c
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-10-13 12:34:40 +0300

    Add some basic App tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -10,8 +10,21 @@ const renderApp = async () => {
 };
 
 describe('App', () => {
-  it('should render counter', async () => {
+  it('should render heading', async () => {
     await renderApp();
     expect(screen.getByRole('heading', { name: /where('s| is) waldo/i })).toBeVisible();
+  });
+
+  it('should render images for Waldo and his friends', async () => {
+    await renderApp();
+    expect(screen.getByRole('img', { name: /of waldo/i })).toBeVisible();
+    expect(screen.getByRole('img', { name: /of odlaw/i })).toBeVisible();
+    expect(screen.getByRole('img', { name: /of wilma/i })).toBeVisible();
+    expect(screen.getByRole('img', { name: /of (wizard|whitebeard)/i })).toBeVisible();
+  });
+
+  it('should render crowded illustration, where we can find Waldo', async () => {
+    await renderApp();
+    expect(screen.getByRole('img', { name: /crowded .* waldo/i })).toBeVisible();
   });
 });
