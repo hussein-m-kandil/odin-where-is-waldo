b0c81a4aa6d6a7e70d75318f0d33799fe8fa14f0
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-10-17 16:43:23 +0300

    Enhance CharacterMenu placement logic

--- src/app/characters/character-selection/character-menu/character-menu.spec.ts
+++ src/app/characters/character-selection/character-menu/character-menu.spec.ts
@@ -4,10 +4,27 @@ import { render, screen } from '@testing-library/angular';
 import { CharacterMenu } from './character-menu';
 import { appConfig } from '../../../app.config';
 
+const rect = new DOMRect(...Object.values({ x: 10, y: 10, w: 1600, h: 900 }));
+const mockImageRect = vi.fn(() => rect);
+const mockPoint = vi.fn(() => ({ x: 0, y: 0 }));
+const mockImage = vi.fn(() => {
+  const image = new Image(160, 90);
+  image.getBoundingClientRect = mockImageRect;
+  return image;
+});
+
+// Make the viewport's sides 10px wider than the image box
+window.innerHeight = rect.bottom + rect.y; // <-y-><----h----><-y->
+window.innerWidth = rect.right + rect.x; // <-x-><----w----><-x->
+const spacing = 8;
+const menuSize = 90;
+const markerSize = 16;
+
 const mockCharacterSelection = vi.fn<() => Partial<InstanceType<typeof CharacterSelection>>>(() => {
-  const x = 0;
-  const y = 0;
-  return { selectedPoint: { absolute: { x, y }, relative: { x, y }, natural: { x, y } } };
+  return {
+    selectedPoint: { absolute: mockPoint(), relative: mockPoint(), natural: mockPoint() },
+    imageElement: mockImage(),
+  };
 });
 
 const renderComponent = async () => {
@@ -16,21 +33,168 @@ const renderComponent = async () => {
       ...appConfig.providers,
       { provide: CharacterSelection, useValue: mockCharacterSelection() },
     ],
+    inputs: { markerSize, menuSize, spacing },
   });
 };
+
+const markerRegex = /marker/i;
+const menuRegex = /menu/i;
 
 describe('CharacterMenu', () => {
   afterEach(vi.clearAllMocks);
 
-  it('should not render anything if the given `selectedPoint` is `null`', async () => {
+  it('should not be rendered if there is no a selected point', async () => {
     mockCharacterSelection.mockImplementationOnce(() => ({ selectedPoint: null }));
     const { container } = await renderComponent();
     expect(container).toBeEmptyDOMElement();
   });
 
-  it('should render character selector if given a `selectedPoint`', async () => {
+  it('should be rendered if there is a selected point', async () => {
     await renderComponent();
-    expect(screen.getByLabelText(/menu/i)).toBeVisible();
-    screen.getByLabelText(/marker/i);
+    expect(screen.getByLabelText(menuRegex)).toBeVisible();
+    expect(screen.getByLabelText(markerRegex)).toBeVisible();
+  });
+
+  it('should render the character images', async () => {
+    await renderComponent();
+    for (const character of ['odlaw', 'waldo', 'wilma', 'wizard']) {
+      expect(screen.getByRole('img', { name: new RegExp(character, 'i') })).toBeVisible();
+    }
+  });
+
+  describe('should be rendered beneath the marker', () => {
+    it('should be rendered in the top-left corner', async () => {
+      const point = { x: 0, y: 0 };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.left).toBe(`${rect.left}px`);
+      expect(markerElement.style.top).toBe(`${rect.top}px`);
+      expect(menuElement.style.left).toBe(`0px`);
+      expect(menuElement.style.top).toBe(`${rect.y + markerSize + spacing}px`);
+    });
+
+    it('should be rendered in the top-center corner', async () => {
+      const point = { x: window.innerWidth * 0.5, y: 0 };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.top).toBe(`${rect.top}px`);
+      expect(markerElement.style.left).toBe(`${point.x - markerSize * 0.5}px`);
+      expect(menuElement.style.top).toBe(`${rect.top + markerSize + spacing}px`);
+      expect(menuElement.style.left).toBe(`${point.x - menuSize * 0.5}px`);
+    });
+
+    it('should be rendered in the top-right corner', async () => {
+      const point = { x: window.innerWidth, y: 0 };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.top).toBe(`${rect.top}px`);
+      expect(markerElement.style.left).toBe(`${rect.right - markerSize}px`);
+      expect(menuElement.style.top).toBe(`${rect.top + markerSize + spacing}px`);
+      expect(menuElement.style.left).toBe(`${point.x - menuSize}px`);
+    });
+
+    it('should be rendered in the top-left quadrant', async () => {
+      const point = { x: window.innerWidth * 0.25, y: window.innerHeight * 0.25 };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.top).toBe(`${point.y - markerSize * 0.5}px`);
+      expect(markerElement.style.left).toBe(`${point.x - markerSize * 0.5}px`);
+      expect(menuElement.style.top).toBe(`${point.y + markerSize * 0.5 + spacing}px`);
+      expect(menuElement.style.left).toBe(`${point.x - menuSize * 0.5}px`);
+    });
+
+    it('should be rendered in the middle', async () => {
+      const point = { x: window.innerWidth * 0.5, y: window.innerHeight * 0.5 };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.top).toBe(`${point.y - markerSize * 0.5}px`);
+      expect(markerElement.style.left).toBe(`${point.x - markerSize * 0.5}px`);
+      expect(menuElement.style.top).toBe(`${point.y + markerSize * 0.5 + spacing}px`);
+      expect(menuElement.style.left).toBe(`${point.x - menuSize * 0.5}px`);
+    });
+
+    it('should be rendered in the top-right quadrant', async () => {
+      const point = { x: window.innerWidth * 0.75, y: window.innerHeight * 0.25 };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.top).toBe(`${point.y - markerSize * 0.5}px`);
+      expect(markerElement.style.left).toBe(`${point.x - markerSize * 0.5}px`);
+      expect(menuElement.style.top).toBe(`${point.y + markerSize * 0.5 + spacing}px`);
+      expect(menuElement.style.left).toBe(`${point.x - menuSize * 0.5}px`);
+    });
+  });
+
+  describe('should be rendered above the marker', () => {
+    it('should be rendered in the bottom-left corner', async () => {
+      const point = { x: 0, y: window.innerHeight };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.left).toBe(`${rect.left}px`);
+      expect(markerElement.style.top).toBe(`${rect.bottom - markerSize}px`);
+      expect(menuElement.style.top).toBe(`${rect.bottom - markerSize - spacing - menuSize}px`);
+      expect(menuElement.style.left).toBe(`0px`);
+    });
+
+    it('should be rendered in the bottom-left quadrant', async () => {
+      const point = { x: window.innerWidth * 0.25, y: window.innerHeight };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.top).toBe(`${rect.bottom - markerSize}px`);
+      expect(markerElement.style.left).toBe(`${point.x - markerSize * 0.5}px`);
+      expect(menuElement.style.top).toBe(`${rect.bottom - markerSize - spacing - menuSize}px`);
+      expect(menuElement.style.left).toBe(`${point.x - menuSize * 0.5}px`);
+    });
+
+    it('should be rendered in the bottom-center corner', async () => {
+      const point = { x: window.innerWidth * 0.5, y: window.innerHeight };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.top).toBe(`${rect.bottom - markerSize}px`);
+      expect(markerElement.style.left).toBe(`${point.x - markerSize * 0.5}px`);
+      expect(menuElement.style.top).toBe(`${rect.bottom - markerSize - spacing - menuSize}px`);
+      expect(menuElement.style.left).toBe(`${point.x - menuSize * 0.5}px`);
+    });
+
+    it('should be rendered in the bottom-right quadrant', async () => {
+      const point = { x: window.innerWidth * 0.75, y: window.innerHeight };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.top).toBe(`${rect.bottom - markerSize}px`);
+      expect(markerElement.style.left).toBe(`${point.x - markerSize * 0.5}px`);
+      expect(menuElement.style.top).toBe(`${rect.bottom - markerSize - spacing - menuSize}px`);
+      expect(menuElement.style.left).toBe(`${point.x - menuSize * 0.5}px`);
+    });
+
+    it('should be rendered in the top-right corner', async () => {
+      const point = { x: window.innerWidth, y: window.innerHeight };
+      mockPoint.mockImplementationOnce(() => point);
+      await renderComponent();
+      const menuElement = screen.getByLabelText(menuRegex);
+      const markerElement = screen.getByLabelText(markerRegex);
+      expect(markerElement.style.top).toBe(`${rect.bottom - markerSize}px`);
+      expect(markerElement.style.left).toBe(`${rect.right - markerSize}px`);
+      expect(menuElement.style.top).toBe(`${rect.bottom - markerSize - spacing - menuSize}px`);
+      expect(menuElement.style.left).toBe(`${point.x - menuSize}px`);
+    });
   });
 });
--- src/app/characters/character-selection/character-menu/character-menu.ts
+++ src/app/characters/character-selection/character-menu/character-menu.ts
@@ -1,7 +1,7 @@
 import { Placement, Point } from '../character-selection.types';
 import { CharacterSelection } from '../character-selection';
+import { Component, inject, input } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
-import { Component, inject } from '@angular/core';
 import { Characters } from '../../characters';
 
 @Component({
@@ -13,6 +13,9 @@ import { Characters } from '../../characters';
 export class CharacterMenu {
   readonly characterSelection = inject(CharacterSelection);
   readonly characters = inject(Characters);
+  readonly markerSize = input(20);
+  readonly menuSize = input(100);
+  readonly spacing = input(8);
 
   protected generatePlacementStyle(placement?: Placement): Record<string, string> {
     if (placement) {
@@ -21,34 +24,42 @@ export class CharacterMenu {
     return {};
   }
 
-  protected calcSelectorMarkerPlacement(relativePoint: Point): Placement {
-    const markerSize = 20;
-    const markerHalfSize = markerSize / 2;
+  protected calcSelectorMarkerPlacement(point: Point): Placement {
     const frame = this.characterSelection.imageElement || document.documentElement;
-    console.log(frame); // TODO: Delete this line!
     const frameRect = frame.getBoundingClientRect();
-    const maxY = frameRect.height - markerSize;
-    const maxX = frameRect.width - markerSize;
-    const minX = 0;
-    const minY = 0;
+    const markerSize = this.markerSize();
+    const markerHalfSize = markerSize / 2;
+    const maxTop = frameRect.bottom - markerSize;
+    const maxLeft = frameRect.right - markerSize;
+    const minLeft = frameRect.left;
+    const minTop = frameRect.top;
     return {
-      left: Math.min(Math.max(relativePoint.x - markerHalfSize, minX), maxX),
-      top: Math.min(Math.max(relativePoint.y - markerHalfSize, minY), maxY),
+      left: Math.min(Math.max(point.x - markerHalfSize, minLeft), maxLeft),
+      top: Math.min(Math.max(point.y - markerHalfSize, minTop), maxTop),
       height: markerSize,
       width: markerSize,
     };
   }
 
   protected calcSelectorMenuPlacement(markerPlacement: Placement): Placement {
-    const menuMargin = 8;
-    const menuDiameter = 100;
-    const menuRadius = menuDiameter / 2;
+    const spacing = this.spacing();
+    const menuSize = this.menuSize();
+    const menuHalfSize = menuSize / 2;
+    const vpWidth = window.innerWidth;
+    const vpHeight = window.innerHeight;
+    const markerTop = markerPlacement.top;
+    const markerBottom = markerTop + markerPlacement.height;
     const markerCenterX = markerPlacement.width / 2 + markerPlacement.left;
+    const markerCenterY = markerPlacement.height / 2 + markerPlacement.top;
+    const maxTop = Math.min(markerBottom + spacing, vpHeight - menuSize);
+    const minTop = Math.max(markerTop - (menuSize + spacing), 0);
+    const maxLeft = vpWidth - menuSize;
+    const minLeft = 0;
     return {
-      top: Math.max(markerPlacement.top - (menuDiameter + menuMargin), 0),
-      left: Math.max(markerCenterX - menuRadius, 0),
-      height: menuDiameter,
-      width: menuDiameter,
+      left: Math.min(Math.max(markerCenterX - menuHalfSize, minLeft), maxLeft),
+      top: markerCenterY <= vpHeight / 2 ? maxTop : minTop,
+      height: menuSize,
+      width: menuSize,
     };
   }
 }
