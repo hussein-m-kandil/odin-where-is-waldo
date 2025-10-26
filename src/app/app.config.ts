009c8467e13414578a8e4d05309ebafe2a4a75c6
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-10-26 09:03:55 +0300

    retry failed requests via an interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,14 @@ import {
   provideZonelessChangeDetection,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { withInterceptors, provideHttpClient } from '@angular/common/http';
+import { retryingInterceptor } from './interceptors/retrying-interceptor';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([retryingInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
